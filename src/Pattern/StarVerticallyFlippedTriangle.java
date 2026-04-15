a7fab87008029f8934504465fe5d8cb4fb430525
Utpal Kumar <utpalkumarsingh2016@gmail.com>
2026-04-15 12:26:24 +0530

    Added

--- src/Pattern/StarVerticallyFlippedTriangle.java
+++ src/Pattern/StarVerticallyFlippedTriangle.java
@@ -4,6 +4,31 @@ import java.util.Scanner;
 
 public class StarVerticallyFlippedTriangle {
     public static void main(String[] args) {
-        Scanner sc
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //Method - 1
+//        for(int i = 1; i<=n;i++){
+//            for(int j = 1;j<=n;j++) {
+//                if((i+j)>n) {
+//                    System.out.print("* ");
+//                }
+//                else{
+//                        System.out.print("  ");
+//                    }
+//            }
+//            System.out.println();
+//        }
+
+        //Method -2
+          for(int i = 1;i<=n;i++){
+              for(int j =1;j<=n-i;j++){
+                  System.out.print("  ");
+              }
+              for(int k=1;k<=i;k++){
+                  System.out.print("* ");
+              }
+              System.out.println();
+          }
     }
 }
