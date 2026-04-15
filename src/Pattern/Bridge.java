f20b7436b81642e4349c8e9405f1857b8a38304d
Utpal Kumar <utpalkumarsingh2016@gmail.com>
2026-04-16 01:27:35 +0530

    Added

--- src/Pattern/Bridge.java
+++ src/Pattern/Bridge.java
@@ -2,29 +2,25 @@ package Pattern;
 
 import java.util.Scanner;
 
-public class Diamond {
+public class Bridge {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 1;i<=n ;i++){
+        for(int x = 1;x<=2*n-1;x++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        for(int i = 1;i<=n-1;i++){
             for(int j = 1;j<=n-i;j++){
+                System.out.print("* ");
+            }
+            for(int k = 1;k<=2*i - 1;k++){
                 System.out.print("  ");
             }
-            for(int k = 1;k<=i*2-1;k++){
+            for(int l = 1;l<=n-i;l++){
                 System.out.print("* ");
             }
             System.out.println();
         }
-        for(int x = 1;x<= n-1;x++){
-            for(int y = 1;y<=x;y++) {
-                System.out.print("  ");
-            }
-
-            for(int z = 1;z<=2*n-2*x-1; z++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
     }
 }
