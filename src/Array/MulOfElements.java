aa0d723bc0a830968f3e8ad6694314ebbd926193
Utpal Kumar <utpalkumarsingh2016@gmail.com>
2026-04-30 23:06:00 +0530

    Practiced

---
+++ src/Array/MulOfElements.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MulOfElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of Array:- ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int Mul = 1;
+        System.out.println("Enter Elements of Array:- ");
+        for(int i=0;i< arr.length;i++){
+            arr[i] = sc.nextInt();
+            Mul *= arr[i];
+        }
+        System.out.println("Multiplication of Elements of Array:- "+Mul);
+    }
+}
