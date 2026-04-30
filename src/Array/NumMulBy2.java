cab1e3139a3d3014c9660a138a6a5f6828082826
Utpal Kumar <utpalkumarsingh2016@gmail.com>
2026-04-30 23:06:38 +0530

    Practiced

---
+++ src/Array/NumMulBy2.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.Scanner;
+
+public class NumMulBy2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[7];
+        //Default Values
+        //for (int i = 0;i<arr.length;i++){
+            //System.out.print(arr[i]+" ");
+        //}
+        //input
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        //print
+        for (int i = 0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
