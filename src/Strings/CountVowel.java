ae73e17e352cc7ed3142b775b0a11ffedacb39f4
Utpal Kumar <utpalkumarsingh2016@gmail.com>
2026-05-29 23:28:20 +0530

    Practiced

--- src/Strings/CountVowel.java
+++ src/Strings/CountVowel.java
@@ -1,5 +1,19 @@
 package Strings;
 
-public class CountVowel {
+import java.util.Scanner;
 
+public class CountVowel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String s = sc.nextLine();
+        int count = 0 ;
+        for (int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                count ++;
+            }
+        }
+        System.out.println("Total Vowel : "+ count);
+    }
 }
